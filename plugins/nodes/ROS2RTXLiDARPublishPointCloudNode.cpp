78b2075a91be66ad798e61302edaeb828049f023
Joe Ferreira Scholtz <joe.scholtz@senairs.org.br>
2024-02-28 08:16:03 -0300

    first commit

---
+++ plugins/nodes/ROS2RTXLiDARPublishPointCloudNode.cpp
@@ -0,0 +1,116 @@
+// Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+//
+// NVIDIA CORPORATION and its licensors retain all intellectual property
+// and proprietary rights in and to this software, related documentation
+// and any modifications thereto.  Any use, reproduction, disclosure or
+// distribution of this software and related documentation without an express
+// license agreement from NVIDIA CORPORATION is strictly prohibited.
+//
+#include <ROS2RTXLiDARPublishPointCloudNodeDatabase.h>
+#include <string>
+
+// In this example, we will publish a string message with an OmniGraph Node
+#include "rosidl_runtime_c/string_functions.h"
+#include "std_msgs/msg/string.h"
+
+// ROS includes for creating nodes, publishers etc.
+#include "rcl/rcl.h"
+
+// Helpers to explicit shorten names you know you will use
+using omni::graph::core::BaseDataType;
+using omni::graph::core::Type;
+
+namespace custom {
+namespace ros2_cpp {
+namespace omnigraph_node {
+
+class ROS2RTXLiDARPublishPointCloudNode {
+public:
+  static bool compute(ROS2RTXLiDARPublishPointCloudNodeDatabase &db) {
+    auto &state = db.internalState<ROS2RTXLiDARPublishPointCloudNode>();
+
+    if (!state.pub_created) {
+      rcl_context_t context = rcl_get_zero_initialized_context();
+      rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
+      rcl_allocator_t allocator = rcl_get_default_allocator();
+      rcl_ret_t rc;
+
+      // create init_options
+      rc = rcl_init_options_init(&init_options, allocator);
+      if (rc != RCL_RET_OK) {
+        printf("Error rcl_init_options_init.\n");
+        return false;
+      }
+
+      // create context
+      rc = rcl_init(0, nullptr, &init_options, &context);
+      if (rc != RCL_RET_OK) {
+        printf("Error in rcl_init.\n");
+        return false;
+      }
+
+      // create rcl_node
+      rcl_node_t my_node = rcl_get_zero_initialized_node();
+      rcl_node_options_t node_ops = rcl_node_get_default_options();
+      rc =
+          rcl_node_init(&my_node, "node_0", "custom_node", &context, &node_ops);
+      if (rc != RCL_RET_OK) {
+        printf("Error in rcl_node_init\n");
+        return false;
+      }
+
+      const char *topic_name = "my_string";
+      const rosidl_message_type_support_t *my_type_support =
+          ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String);
+
+      rcl_publisher_options_t pub_options = rcl_publisher_get_default_options();
+
+      // Initialize Publisher
+      rc = rcl_publisher_init(&state.my_pub, &my_node, my_type_support,
+                              topic_name, &pub_options);
+      if (RCL_RET_OK != rc) {
+        printf("Error in rcl_publisher_init %s.\n", topic_name);
+        return false;
+      }
+      // Node, publisher was successfully created
+      state.pub_created = true;
+
+      return true;
+    }
+
+    std_msgs__msg__String *ros_msg = std_msgs__msg__String__create();
+
+    // Get the string to be published
+    std::string msg = db.inputs.frameId();
+
+    // Assign our ros message the data from this string
+    rosidl_runtime_c__String__assign(&ros_msg->data, msg.c_str());
+
+    rcl_ret_t rc;
+    rc = rcl_publish(&state.my_pub, ros_msg, NULL);
+    if (rc != RCL_RET_OK) {
+      // RCL_RET_PUBLISHER_INVALID is returned initially and then the message
+      // gets published
+      return false;
+    }
+
+    // Destroy the ROS message published to release the memory it used
+    std_msgs__msg__String__destroy(ros_msg);
+
+    // Returning true tells Omnigraph that the compute was successful and the
+    // output value is now valid.
+    return true;
+  }
+
+private:
+  rcl_publisher_t my_pub;
+  bool pub_created{false};
+};
+
+// This macro provides the information necessary to OmniGraph that lets it
+// automatically register and deregister your node type definition.
+REGISTER_OGN_NODE()
+
+} // namespace omnigraph_node
+} // namespace ros2_cpp
+} // namespace custom
---
+++ premake5.lua
@@ -0,0 +1,78 @@
+-- Setup the basic extension information.
+local ext = get_current_extension_info()
+project_ext(ext)
+
+
+-- --------------------------------------------------------------------------------------------------------------
+-- Helper variable containing standard configuration information for projects containing OGN files.
+local ogn = get_ogn_project_information(ext, "omni/isaac/ros2_cpp_custom_og_node")
+
+
+-- --------------------------------------------------------------------------------------------------------------
+-- Link folders that should be packaged with the extension.
+repo_build.prebuild_link {
+  { "data", ext.target_dir .. "/data" },
+  { "docs", ext.target_dir .. "/docs" },
+}
+
+
+-- --------------------------------------------------------------------------------------------------------------
+-- Copy the __init__.py to allow building of a non-linked ogn/ import directory.
+-- In a mixed extension this would be part of a separate Python-based project but since here it is just the one
+-- file it can be copied directly with no build dependencies.
+repo_build.prebuild_copy {
+  { "omni/isaac/ros2_cpp_custom_og_node/__init__.py", ogn.python_target_path }
+}
+
+-- --------------------------------------------------------------------------------------------------------------
+-- Breaking this out as a separate project ensures the .ogn files are processed before their results are needed.
+project_ext_ogn(ext, ogn)
+
+
+
+-- --------------------------------------------------------------------------------------------------------------
+-- Build the C++ plugin that will be loaded by the extension.
+project_ext_plugin(ext, ogn.plugin_project)
+-- It is important that you add all subdirectories containing C++ code to this project
+add_files("source", "plugins/" .. ogn.module)
+add_files("nodes", "plugins/nodes")
+
+-- Add the standard dependencies all OGN projects have; includes, libraries to link, and required compiler flags
+add_ogn_dependencies(ogn)
+
+includedirs {
+  -- System level ROS includes
+  "%{root}/_build/target-deps/system_ros/include/std_msgs",
+
+  "%{root}/_build/target-deps/system_ros/include/geometry_msgs",
+
+  "%{root}/_build/target-deps/system_ros/include/rosidl_runtime_c",
+
+  "%{root}/_build/target-deps/system_ros/include/rosidl_typesupport_interface",
+
+  "%{root}/_build/target-deps/system_ros/include/rcl",
+
+  "%{root}/_build/target-deps/system_ros/include/rcutils",
+
+  "%{root}/_build/target-deps/system_ros/include/rmw",
+
+  "%{root}/_build/target-deps/system_ros/include/rcl_yaml_param_parser",
+
+}
+
+libdirs {
+  -- System level ROS libraries
+  "%{root}/_build/target-deps/system_ros/lib",
+}
+
+links {
+  --  Minimal ROS 2 C API libs needed for your nodes to work
+  "rosidl_runtime_c", "rcutils", "rcl", "rmw",
+
+  -- For the simple string message, add the deps
+  "std_msgs__rosidl_typesupport_c", "std_msgs__rosidl_generator_c",
+}
+
+
+filter { "system:linux" }
+linkoptions { "-Wl,--export-dynamic" }
